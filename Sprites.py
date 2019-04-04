08d6e1223dd0950f96bd933cf9c453a413993bdc
ElleChan <programmer.jael.butler@gmail.com>
2019-04-04 11:40:06 -0700

    Updated sprite template

--- Sprites.py
+++ Sprites.py
@@ -8,6 +8,7 @@ class Sprite:
         self._images = images                                                   # List/array of the images to use to render sprite.
         self._current_x = start_x
         self._current_y = start_y
+        self._current_image                                                     # The current image that the sprite should be drawn as.
 
     # If sprite can move left, decrement x by 1. Else, don't move.
     def move_left(self):
@@ -25,10 +26,13 @@ class Sprite:
     def move_down(self):
         pass
 
-    # Draw sprite to screen surface object.
-    def draw_sprite(self, image):
-        pass
+    # Returns the current position of the sprite as a tuple (i.e. coordinate)
+    def current_position(self):
+        return (self._current_x, self._current_y)
 
+    # Draws the sprite to a surface object. Maybe move this to the game board class and just get the coords and image instead?
+    def draw_sprite(self, surfaceObj):
+        pass
 
 class Ghost(Sprite):
     def __init__(self, images, start_x, start_y):
