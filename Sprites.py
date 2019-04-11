61b017142fed854fc8f069819ec7e620d50c39f2
ElleChan <programmer.jael.butler@gmail.com>
2019-04-10 18:34:59 -0700

    removed some methods

--- Sprites.py
+++ Sprites.py
@@ -30,17 +30,6 @@ class Sprite:
         if self.can_move(self._current_x, self._current_y + 1):
             self._current_y += 1
 
-    # If sprite can move to (x,y) from (current_x, current_y), return true, else false.
-    def can_move(self, x, y):
-        pass
-
-    # Returns the current position of the sprite as a tuple (i.e. coordinate)
-    def current_position(self):
-        return (self._current_x, self._current_y)
-
-    # Draws the sprite to a surface object. Maybe move this to the game board class and just get the coords and image instead?
-    def draw_sprite(self, surfaceObj):
-        pass
 
 
 class Ghost(Sprite):
